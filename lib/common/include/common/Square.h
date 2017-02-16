0d6f4120f270f0213833aafff8d7ffd6ba4603d7
Ondra Tethal <tethal@email.cz>
2017-02-16 10:22:34 +0100

    Added support for doxygen

--- lib/common/include/common/Square.h
+++ lib/common/include/common/Square.h
@@ -1,8 +1,19 @@
+//------------------------------------------------------------------------------
+///
+/// \file
+/// \brief Defines a sample function.
+///
+//------------------------------------------------------------------------------
 #ifndef COMMON_SQUARE_H_
 #define COMMON_SQUARE_H_
 
 namespace crypto {
 
+/**
+ * \brief Calculates the square of a given number.
+ * \param x the number to square
+ * \return the square of \c x
+ */
 int square(int x);
 
 } // namespace crypto

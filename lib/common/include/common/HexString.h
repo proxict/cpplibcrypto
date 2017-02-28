efee349bf8ca47196bbcf694ec6198bb4e7195ab
Marek Bogusovsky <bobogu@protonmail.com>
2017-02-28 17:46:57 +0100

    Added documentation for Hex and HexString

--- lib/common/include/common/HexString.h
+++ lib/common/include/common/HexString.h
@@ -1,3 +1,9 @@
+//------------------------------------------------------------------------------
+///
+/// \file
+/// \brief Defines a class reprezenation of base16 string
+///
+//------------------------------------------------------------------------------
 #ifndef COMMON_HEX_STRING_H_
 #define COMMON_HEX_STRING_H_
 
@@ -6,29 +12,62 @@
 
 namespace crypto {
 
+/**
+ * \brief class reprezenation of base16 string
+ */
 class HexString {
 public:
+    /**
+     * \brief HexString constructor
+     * \param hexStr hexadecimal string
+     * \throws std::invalid_argument if hexStr contains invalid hexadecimal character
+     */
     HexString(const std::string& hexStr) : m_decoded(Hex::decode(hexStr)) {}
 
+    /**
+     * \brief operator= move assignment operator
+     * \param other HexString instance
+     * \return HexString object
+     */
     HexString& operator=(HexString&& other) noexcept {
         m_decoded = std::move(other.m_decoded);
         return *this;
     }
 
+    /**
+     * \brief HexString move constructor
+     * \param other HexString instance
+     */
     HexString(HexString&& other) noexcept {
         *this = std::move(other);
     }
 
+    /**
+     * \brief operator+= append operator
+     * \param rhs HexString object
+     * \return HexString object
+     */
     HexString& operator+=(const HexString& rhs) {
         m_decoded += rhs.m_decoded;
         return *this;
     }
 
+    /**
+     * \brief operator+= append HexString to ByteBuffer
+     * \param lhs ByteBuffer instance
+     * \param rhs HexString instance
+     * \return ByteBuffer object
+     */
     friend ByteBuffer& operator+=(ByteBuffer& lhs, const HexString& rhs) {
         lhs += rhs.m_decoded;
         return lhs;
     }
 
+    /**
+     * \brief operator+ adding two HexStrings
+     * \param rhs HexString instance
+     * \return HexString object
+     */
     const HexString operator+(const HexString& rhs) const {
         HexString hexString("");
         hexString += *this;
@@ -36,6 +75,11 @@ public:
         return hexString;
     }
 
+    /**
+     * \brief operator== compares if two HexStrings are equal
+     * \param rhs HexString object
+     * \return true if HexStrings are equal, false otherwise
+     */
     bool operator==(const HexString& rhs) const {
         return m_decoded == rhs.m_decoded;
     }

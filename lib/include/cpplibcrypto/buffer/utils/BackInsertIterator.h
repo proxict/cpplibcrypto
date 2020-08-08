cc55853b0d92d1b97fecdca77b3830db06eecbc4
proxict <bobogu@protonmail.com>
2020-08-08 19:39:53 +0200

    Added BackInsertIterator

---
+++ lib/include/cpplibcrypto/buffer/utils/BackInsertIterator.h
@@ -0,0 +1,83 @@
+#ifndef CPPLIBCRYPTO_BUFFER_UTILS_BACKINSERTITERATOR_H_
+#define CPPLIBCRYPTO_BUFFER_UTILS_BACKINSERTITERATOR_H_
+
+#include "cpplibcrypto/common/TypeTraits.h"
+#include "cpplibcrypto/common/common.h"
+
+#include <cstddef>
+#include <iterator>
+
+NAMESPACE_CRYPTO_BEGIN
+
+template <typename TBuffer>
+class BackInsertIterator {
+public:
+    using ValueType = typename TBuffer::ValueType;
+    using Reference = ValueType&;
+    using ConstReference = const ValueType&;
+    using RValuetReference = ValueType&&;
+    using Pointer = ValueType*;
+    using ConstPointer = const ValueType*;
+
+    using iterator_category = std::output_iterator_tag;
+    using value_type = ValueType;
+    using difference_type = ptrdiff_t;
+    using pointer = value_type*;
+    using reference = value_type&;
+
+    constexpr BackInsertIterator() noexcept = default;
+
+    explicit constexpr BackInsertIterator(TBuffer& buffer)
+        : mBuffer(std::addressof(buffer)) {}
+
+    constexpr BackInsertIterator& operator=(const ValueType& value) {
+        pushBack(value);
+        return *this;
+    }
+
+    constexpr BackInsertIterator& operator=(ValueType&& value) {
+        pushBack(std::move(value));
+        return *this;
+    }
+
+    constexpr BackInsertIterator& operator*() { return *this; }
+
+    constexpr BackInsertIterator& operator++() { return *this; }
+
+    constexpr BackInsertIterator operator++(int) { return *this; }
+
+private:
+    template <typename T,
+              typename TBuf = TBuffer,
+              EnableIf<std::is_same<void, decltype(std::declval<TBuf>().push(ValueType()))>::value, char> = 0>
+    constexpr void pushBack(T&& value) {
+        mBuffer->push(std::forward<T>(value));
+    }
+
+    template <
+        typename T,
+        typename TBuf = TBuffer,
+        EnableIf<std::is_same<void, decltype(std::declval<TBuf>().pushBack(ValueType()))>::value, char> = 0>
+    constexpr void pushBack(T&& value) {
+        mBuffer->pushBack(std::forward<T>(value));
+    }
+
+    template <
+        typename T,
+        typename TBuf = TBuffer,
+        EnableIf<std::is_same<void, decltype(std::declval<TBuf>().push_back(ValueType()))>::value, char> = 0>
+    constexpr void pushBack(T&& value) {
+        mBuffer->push_back(std::forward<T>(value));
+    }
+
+    TBuffer* mBuffer = nullptr;
+};
+
+template <typename TBuffer>
+constexpr BackInsertIterator<TBuffer> backInserter(TBuffer& buffer) {
+    return BackInsertIterator<TBuffer>(buffer);
+}
+
+NAMESPACE_CRYPTO_END
+
+#endif // CPPLIBCRYPTO_BUFFER_UTILS_BACKINSERTITERATOR_H_
---
+++ test/buffer/BackInserterTest.cpp
@@ -0,0 +1,72 @@
+#include "gmock/gmock.h"
+
+#include "cpplibcrypto/buffer/DynamicBuffer.h"
+#include "cpplibcrypto/buffer/utils/BackInsertIterator.h"
+
+#include <algorithm>
+
+NAMESPACE_CRYPTO_BEGIN
+
+enum class Functions { UNDEFINED, PUSH, PUSHBACK, PUSH_BACK };
+template <Functions TOverload>
+struct Container {
+    using ValueType = int;
+    static constexpr Functions CURRENT_OVERLOAD = TOverload;
+
+    template <Functions Fun = TOverload, EnableIf<Fun == Functions::PUSH, char> = 0>
+    void push(const ValueType& value) {
+        mBuffer.push(value);
+        mCalledOverload = Functions::PUSH;
+    }
+
+    template <Functions Fun = TOverload, EnableIf<Fun == Functions::PUSHBACK, char> = 0>
+    void pushBack(const ValueType& value) {
+        mBuffer.push(value);
+        mCalledOverload = Functions::PUSHBACK;
+    }
+
+    template <Functions Fun = TOverload, EnableIf<Fun == Functions::PUSH_BACK, char> = 0>
+    void push_back(const ValueType& value) {
+        mBuffer.push(value);
+        mCalledOverload = Functions::PUSH_BACK;
+    }
+
+    std::size_t size() const { return mBuffer.size(); }
+
+    const ValueType& at(const std::size_t index) const { return mBuffer.at(index); }
+
+    DynamicBuffer<ValueType> mBuffer;
+    Functions mCalledOverload = Functions::UNDEFINED;
+};
+
+template <typename T>
+class BackInsertTest : public ::testing::Test {};
+
+TYPED_TEST_SUITE_P(BackInsertTest);
+
+TYPED_TEST_P(BackInsertTest, fill) {
+    TypeParam c;
+    std::fill_n(backInserter(c), 3, -1);
+
+    EXPECT_EQ(DynamicBuffer<int>(3, -1), c.mBuffer);
+    ASSERT_EQ(TypeParam::CURRENT_OVERLOAD, c.mCalledOverload);
+}
+
+TYPED_TEST_P(BackInsertTest, increment) {
+    TypeParam c;
+    auto it = backInserter(c);
+    for (int i = 0; i < 5; ++i) {
+        it++ = i;
+    }
+
+    EXPECT_EQ(DynamicBuffer<int>({ 0, 1, 2, 3, 4 }), c.mBuffer);
+    ASSERT_EQ(TypeParam::CURRENT_OVERLOAD, c.mCalledOverload);
+}
+
+using ContainerTypes = ::testing::
+    Types<Container<Functions::PUSH>, Container<Functions::PUSHBACK>, Container<Functions::PUSH_BACK>>;
+
+REGISTER_TYPED_TEST_SUITE_P(BackInsertTest, fill, increment);
+INSTANTIATE_TYPED_TEST_SUITE_P(BackInserter, BackInsertTest, ContainerTypes);
+
+NAMESPACE_CRYPTO_END
